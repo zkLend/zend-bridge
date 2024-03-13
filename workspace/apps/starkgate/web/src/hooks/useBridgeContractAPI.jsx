25030670bc5090adf6b4f23cff512ff6eedfdbf7
9oelM <hj923@hotmail.com>
2024-03-14 07:51:15 +0800

    feat: L1 to L2 deposit using MultiBridge

--- workspace/apps/starkgate/web/src/hooks/useBridgeContractAPI.jsx
+++ workspace/apps/starkgate/web/src/hooks/useBridgeContractAPI.jsx
@@ -1,6 +1,5 @@
 import {BigNumber} from 'ethers';
 import {useCallback} from 'react';
-import {num} from 'starknet';
 
 import {RELAYER_CONTRACT_ADDRESS} from '@config/envs';
 import {
@@ -11,7 +10,7 @@ import {
   useSendEthereumTransaction,
   useTeleportOracleAuthContract
 } from '@hooks';
-import {useL1Token, useL2Token, useSelectedToken, useWallets} from '@providers';
+import {useEthereumWallet, useL1Token, useSelectedToken, useWallets} from '@providers';
 import {Tokens} from '@starkgate/shared';
 import {promiseHandler} from '@starkware-webapps/utils';
 import {
@@ -23,71 +22,65 @@ import {
 } from '@starkware-webapps/web3-utils';
 import {isDai} from '@utils';
 
-const {hexToDecimalString} = num;
-
 export const useBridgeContractAPI = () => {
-  const {ethereumAccount, getStarknetSigner, getStarknetProvider} = useWallets();
+  const {ethereumAccount, getStarknetSigner} = useWallets();
   const sendEthereumTransaction = useSendEthereumTransaction();
   const selectedToken = useSelectedToken();
   const getL1BridgeContract = useL1TokenBridgeContract();
   const getL1Token = useL1Token();
-  const getL2Token = useL2Token();
   const {TELEPORT_FEE_MULTIPLIER} = useConstants();
   const getTeleportOracleAuthContract = useTeleportOracleAuthContract();
   const fetchGasCost = useGasCost();
   const {SUPPORTED_L2_CHAIN_ID, DAI_TELEPORT_GATEWAY_CONTRACT_ADDRESS, DAI_TELEPORT_TARGET_DOMAIN} =
     useEnvs();
 
-  const estimateMessageFee = useCallback(
-    async (recipient, amount) => {
-      const provider = getStarknetProvider();
-      const {bridgeAddress: l1bridgeAddress, decimals, symbol} = selectedToken;
-      const {low, high} = parseToUint256(amount, decimals);
-      const payload = [recipient, low, high];
-      isDai(symbol) && payload.push(hexToDecimalString(ethereumAccount));
-      const {bridgeAddress: l2bridgeAddress} = getL2Token(symbol);
-      const {overall_fee: estimatedFee} = await provider.estimateMessageFee({
-        from_address: l1bridgeAddress,
-        to_address: l2bridgeAddress,
-        entry_point_selector: 'handle_deposit',
-        payload
-      });
-      return Number(estimatedFee);
-    },
-    [selectedToken, getStarknetProvider, ethereumAccount, getL2Token]
-  );
+  const {getEthereumProvider} = useEthereumWallet();
+  const estimateDepositFeeWeiFromL1Bridge = useCallback(async contract => {
+    const ethProvider = getEthereumProvider();
+    if (ethProvider) {
+      const [estimatedDepositFee, error] = await promiseHandler(contract.estimateDepositFeeWei());
+      if (error) {
+        return Promise.reject(error);
+      }
+      return estimatedDepositFee;
+    }
+  }, []);
 
   const deposit = useCallback(
     async ({recipient, amount}) => {
       const {bridgeAddress, decimals} = selectedToken;
       const contract = await getL1BridgeContract(bridgeAddress);
-      const estimatedL2Fee = await estimateMessageFee(recipient, amount);
+      const estimatedDepositFeeWei = await estimateDepositFeeWeiFromL1Bridge(contract);
       return await sendEthereumTransaction({
         contract,
-        method: 'deposit(uint256,uint256)',
-        args: [parseToDecimals(amount, decimals), recipient],
+        // address token,
+        // uint256 amount,
+        // uint256 l2Recipient
+        method: 'deposit(address,uint256,uint256)',
+        args: [selectedToken.tokenAddress, parseToDecimals(amount, decimals), recipient],
         transaction: {
           from: ethereumAccount,
-          value: BigNumber.from(estimatedL2Fee).toString()
+          value: BigNumber.from(estimatedDepositFeeWei).toString()
         }
       });
     },
     [selectedToken, ethereumAccount, getL1BridgeContract, sendEthereumTransaction]
   );
 
+  // This function is not used anyway for zkLend, it's just a placeholder
+  // to prevent any other things from breaking.
   const depositEth = useCallback(
     async ({recipient, amount}) => {
       const {bridgeAddress} = selectedToken;
       const contract = await getL1BridgeContract(bridgeAddress);
       const parsedAmount = parseToDecimals(amount);
-      const estimatedL2Fee = await estimateMessageFee(recipient, amount);
       return await sendEthereumTransaction({
         contract,
         method: 'deposit(uint256,uint256)',
         args: [parsedAmount, recipient],
         transaction: {
           from: ethereumAccount,
-          value: BigNumber.from(parsedAmount).add(estimatedL2Fee).toString()
+          value: BigNumber.from(parsedAmount).toString()
         }
       });
     },
@@ -123,26 +116,24 @@ export const useBridgeContractAPI = () => {
     [selectedToken, getL1BridgeContract]
   );
 
-  const maxTotalBalance = useCallback(
-    async () => {
-      // Multibridge at
-      // https://etherscan.io/address/0xF5b6Ee2CAEb6769659f6C091D209DfdCaF3F69Eb#readProxyContract
-      // doesn't have maxTotalBalance anymore, but strangely enough,
-      // it still tries to get maxTotalBalance. So just ignore this.
+  const maxTotalBalance = useCallback(async () => {
+    // Multibridge at
+    // https://etherscan.io/address/0xF5b6Ee2CAEb6769659f6C091D209DfdCaF3F69Eb#readProxyContract
+    // doesn't have maxTotalBalance anymore, but the legacy bridge has maxTotalBalance.
+    // So let this function just return the max value of uint256 for a quick fix that works
+    // without having to change other lines of code.
 
-      // const {bridgeAddress, decimals, symbol} = token || selectedToken;
-      // const contract = await getL1BridgeContract(bridgeAddress);
+    // const {bridgeAddress, decimals, symbol} = token || selectedToken;
+    // const contract = await getL1BridgeContract(bridgeAddress);
 
-      // const [maxTotalBalance, error] = await promiseHandler(
-      //   isDai(symbol) ? contract.ceiling() : contract.maxTotalBalance()
-      // );
-      // if (error) {
-      //   return Promise.reject(error);
-      // }
-      return '115792089237316195423570985008687907853269984665640564039457.584007913129639935';
-    },
-    []
-  );
+    // const [maxTotalBalance, error] = await promiseHandler(
+    //   isDai(symbol) ? contract.ceiling() : contract.maxTotalBalance()
+    // );
+    // if (error) {
+    //   return Promise.reject(error);
+    // }
+    return '115792089237316195423570985008687907853269984665640564039457.584007913129639935';
+  }, []);
 
   const initiateWithdraw = useCallback(
     async ({recipient, amount, autoWithdrawal}) => {
